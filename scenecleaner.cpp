225057a6c920e0f736820998e2cab368484b6b21
Koala <johnpa9191@gmail.com>
2023-01-06 20:57:48 +0800

    update(2023/1/6)

--- scenecleaner.cpp
+++ scenecleaner.cpp
@@ -11,12 +11,12 @@ QImage SceneCleaner::erased(QPoint pos, QImage img,QPen pen)
 {
     QImage EraseImg(img.size(),QImage::Format_ARGB32_Premultiplied);
 
-
     QPainter painter(&EraseImg);
-    painter.drawImage(0,0,img);
-    painter.setRenderHint(QPainter::Antialiasing, true);
+    painter.setRenderHint(QPainter::Antialiasing,true);
     painter.setCompositionMode(QPainter::CompositionMode_Source);   
-
+    painter.setBackgroundMode(Qt::OpaqueMode);
+    painter.setBackground(Qt::transparent);
+    painter.drawImage(0,0,img);
     painter.setPen(pen);
 
     painter.drawPoint(pos);
@@ -30,10 +30,11 @@ QImage SceneCleaner::erased(QPoint pos1, QPoint pos2, QImage img,QPen pen)
     QImage EraseImg(img.size(),QImage::Format_ARGB32_Premultiplied);
 
     QPainter painter(&EraseImg);
+    painter.setRenderHint(QPainter::Antialiasing,true);
+    painter.setBackgroundMode(Qt::OpaqueMode);
+    painter.setBackground(Qt::transparent);
+    painter.setCompositionMode(QPainter::CompositionMode_Source);
     painter.drawImage(0,0,img);
-    painter.setRenderHint(QPainter::Antialiasing, true);
-    painter.setCompositionMode(QPainter::CompositionMode_Source);    
-
     painter.setPen(pen);
 
     painter.drawLine(pos1,pos2);
